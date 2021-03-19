6be24c5178f9c0412e93211093baba73d2eafa46
Melvin-Leroy <melv.leroy@gmail.com>
2021-03-19 14:41:41 +0100

    Moove rearrangefiles

--- Python_files/rearrangefiles.py
+++ Python_files/rearrangefiles.py
@@ -7,33 +7,11 @@
 
 import os
 import shutil
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
 import pandas as pd
-import seaborn as sns
-from IPython.display import display 
-
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-from torchvision import datasets
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-
 
 
 # directory after downloading images
-datpath = './Mole_Data'
+datpath = '../data/Mole_Data/'
 # bitmat images
 print(os.listdir(datpath)) 
 dir1 = os.listdir(datpath) 
@@ -41,35 +19,19 @@ dir2 = ['SET_D', 'SET_E', 'SET_F']
 # list files in 'SET_E'
 os.listdir(datpath + '/' + dir2[1])
 
-
 # Pandas
-dat = pd.read_csv('./Mole_Data/CLIN_DIA.csv')
-dat.head()
-
-dat.columns
-dat.shape
-
-list(dat.columns.values)
-# ['klin. Diagn.', 'id', 'nr', 'Histo performed', 'Diagnose red.', 'kat.Diagnose']
+dat = pd.read_csv('../data/Mole_Data/CLIN_DIA.csv')
 
 dat.columns = ['klindiag', 'img', 'nr', 'histo', 'diagred', 'cat']
-dat.head()
-
 
 # shape (3000,2)
 datimg = dat[['img','cat']]
 datimg.loc[:,'img'] = datimg.img + '.bmp'
-datimg.head()
-datimg.isnull().sum()
 
 datimg = datimg[(datimg.cat != '?')]
 
-
 datimg.loc[:,'cat'] = datimg['cat'].astype(int)
-print(datimg.dtypes)
-datimg.cat.value_counts()
-# 1 2300; 2 585; 0 62; 3 52; 2300+585+62+52 = 2999 rows
-
+"""
 # a function to do this job would be better !
 for i in range(len(dir2)):    
     images = os.listdir(datpath + '/' + dir2[i])
@@ -80,9 +42,6 @@ for i in range(len(dir2)):
             x = item.lower()
             cat = datimg.loc[datimg.img == x].cat.item()
             # put here your destination folder
-            dstp = './myfolder/' + str(cat) + '/'
+            dstp = '../data/Mole_Data_Rearranged' + str(cat) + '/'
             shutil.copy(srcp + item, dstp + item)
-
-
-
-
+"""
